902d8d839b9935574905dcddf470ba638ce9cb25
sam1212111 <116268024+sam1212111@users.noreply.github.com>
2022-11-30 18:48:58 +0530

    Update pythonemailslicing.py

--- pythonemailslicing.py
+++ pythonemailslicing.py
@@ -1,7 +1,8 @@
-print("----------------------------------------------------------------------EMAIL SLICER------------------------------------------------------------------------------")
-print("-----------------------------------------THIS IS A PROGRAM WHERE YOU CAN SEPERATE A EMAIL INTO USERNAME AND DOMAIN NAME-----------------------------------------")
-z=1
-n = 0
+print("----------------------------------------------------------------------EMAIL SLICER-----------------------------------------------------------------")
+print("-----------------------------------------THIS IS A PROGRAM WHERE YOU CAN SEPERATE A EMAIL INTO USERNAME AND DOMAIN NAME----------------------------")
+print()
+print()
+n= 0
 import re
 while n<3:
         s = input("Enter your Email: ")
@@ -11,18 +12,28 @@ while n<3:
                 break
         else:
             print("Invalid Email")
-            print("Enter again")
             n+=1
-            continue
+            if n==1:
+                print("Enter again")
+            if n==2:
+                print("Enter again")
+            if n==3:
+                print("3 invalid attempts, please try again later")
+                continue
+
+            
+
 a = n
 h = 0
+
+        
 while a!=3 and h<1:
         print()
-        print("__________________________________________________________________________MAIN MENU_____________________________________________________________________________")
-        print(">Press 1 to Print Username: ")
-        print(">Press 2 to Print Domain name: ")
-        print(">Press 3 to Print Both: ")
-        print(">Press 0 to Exit: ")
+        print("_________________________________________________________________________MAIN MENU_________________________________________________________________")
+        print(">Enter 1 to Print Username")
+        print(">Enter 2 to Print Domain name")
+        print(">Enter 3 to Print Both")
+        print(">Enter 0 to Exit")
         print()
         h+=1
         while True:
@@ -34,12 +45,13 @@ while a!=3 and h<1:
                 elif a=="2":
                         print("Domain Name: "+c)
                 elif a=="3":
-                        print("User Name: "+s[:b],"  Domain Name:"+c)
+                        print("User Name: "+s[:b],"  Domain Name: "+c)
                 elif a=="0":
+                        print("~~~~~~~~~~EXITED PROGRAM~~~~~~~~~~")
                         z=0
                         break
                 else:
                         print("Input error")
-print("------------------------------------------------------------------------------------------THANK YOU-------------------------------------------------------------")
-
-        
+print()
+print()
+print("----------------------------------------------------------------------------THANK YOU--------------------------------------------------------------")
